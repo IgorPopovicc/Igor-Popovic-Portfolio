49eb3cf8866c80ce84a1d415e4e80bc56855262f
igpopovic <igorpopovic999@gmail.com>
2025-09-06 23:06:24 +0200

    feat: add main components and styles variables;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Services } from './pages/services/services';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Home | Igor Popovic' },
+  { path: 'services', component: Services, title: 'Services | Igor Popovic' },
+  { path: 'about', component: About, title: 'About | Igor Popovic' },
+  { path: 'contact', component: Contact, title: 'Contact | Igor Popovic' },
+  { path: '**', redirectTo: '' }
+];
